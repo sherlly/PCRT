60ce4e1df6da6f23e9401adce60e2ef2a8ec3b44
sherlly <915279169@qq.com>
2017-08-23 14:23:17 +0800

    update by sherlly

--- PCRT.py
+++ PCRT.py
@@ -1,6 +1,6 @@
 # -*- coding:utf-8 -*-
 __author__="sherlly"
-__version__="1.0"
+__version__="1.1"
 
 import zlib
 import struct
@@ -48,6 +48,12 @@ def str2hex(s):
 
 def int2hex(i):
 	return '0x'+hex(i)[2:].upper()
+
+def str2num(s,n=0):
+	if n==4:
+		return struct.unpack('!I',s)[0]
+	else:
+		return eval('0x'+str2hex(s))
 
 def WriteFile(filename):
 	if os.path.isfile(filename)==True:
@@ -97,12 +103,13 @@ def Termcolor(flag,sentence):
 
 
 class PNG(object):
-	
-	def __init__(self,in_file,out_file='output.png',choices='',mode=0):
+
+	def __init__(self,in_file='',out_file='output.png',choices='',mode=0):
 		self.in_file=in_file
 		self.out_file=out_file
 		self.choices=choices
-		self.mode=mode
+		self.i_mode=mode
+
 	
 	def __del__(self):
 		try:
@@ -110,13 +117,305 @@ class PNG(object):
 		except AttributeError:
 			pass
 
-	def CheckPNG(self):
+	def AddPayload(self,name,payload,way):
+		data=self.LoadPNG()
+		if data==-1:
+			return -1
+		self.file=WriteFile(self.out_file)
+		if way==1:
+			# way1:add ancillary
+			payload_chunk=self.MakeAncillary(name,payload)
+			pos=data.find('IHDR')
+			self.file.write(data[:pos+21])
+			self.file.write(payload_chunk)
+			self.file.write(data[pos+21:])
+		elif way==2:
+			# way2:add critical chunk:IDAT
+			name='IDAT'
+			payload_chunk=self.MakeCritical(name,payload)
+			pos=data.find('IEND')
+			self.file.write(data[:pos-4])
+			self.file.write(payload_chunk)
+			self.file.write(data[pos-4:])
+		
+	def MakeCritical(self,name,payload):
+		print Termcolor('Notice','Payload chunk name: %s'%name)
+		payload=zlib.compress(payload)
+		length=len(payload)
+		crc=zlib.crc32(name+payload) & 0xffffffff
+		data=struct.pack('!I4s%dsI'%(length),length,name,payload,crc)
+		return data
+
+	def MakeAncillary(self,name,payload):
+		if name==None:
+			name=self.RanAncillaryName()
+		name=name[0].lower()+name[1:4].upper()
+		print Termcolor('Notice','Payload chunk name: %s'%name)
+		length=len(payload)
+		crc=zlib.crc32(name+payload) & 0xffffffff
+		data=struct.pack('!I4s%dsI'%(length),length,name,payload,crc)
+		return data
+
+	def RanAncillaryName(self):
+		import random,string
+		key = 'abcdefghijklmnopqrstuvwxyz'
+		name=''.join(random.sample(string.ascii_lowercase,4))
+		return name
+
+	def GetPicInfo(self,ihdr=''):
+		'''
+		bits: color depth
+		mode: 0:gray[1] 2:RGB[3] 3:Indexed[1](with palette) 4:grey & alpha[2] 6:RGBA[4]
+		compression: DEFLATE(LZ77+Huffman)
+		filter: 0:None 1:sub X-A 2:up X-B 3:average X-(A+B)/2 4:Paeth p = A + B − C
+		C B D
+		A X
+		'''
+		data=self.LoadPNG()
+		if data==-1:
+			return -1
+		if ihdr=='':
+			pos,IHDR=self.FindIHDR(data)
+			if pos==-1:
+				print Termcolor('Detected','Lost IHDR chunk')
+				return -1
+			length=struct.unpack('!I',IHDR[:4])[0]
+			ihdr=IHDR[8:8+length]
+
+		self.width,self.height,self.bits,self.mode,self.compression,self.filter,self.interlace=struct.unpack('!iiBBBBB',ihdr)
+
+		self.interlace=str2num(ihdr[12])
+		if self.mode==0 or self.mode==3: # Gray/Index
+			self.channel=1
+		elif self.mode==2: # RGB
+			self.channel=3
+		elif self.mode==4: #GA
+			self.channel=2
+		elif self.mode==6: # RGBA
+			self.channel=4
+		else:
+			self.channel=0
+
+		data=self.LoadPNG()
+		if data==-1:
+			return -1
+		self.content=self.FindAncillary(data)
+
+	def PrintPicInfo(self):
+		status=self.GetPicInfo()
+		if status==-1:
+			return -1
+
+		mode_dict={0:'Grayscale',2:'RGB',3:'Indexed',4:'Grayscale with Alpha',6:'RGB with Alpha'}
+		compress_dict={0:'Deflate'}
+		filter_dict={0:'None',1:'Sub',2:'Up',3:'Average',4:'Paeth'}
+		interlace_dict={0:'Noninterlaced',1:'Adam7 interlaced'}
+		print '\n-------------------------Image Infomation---------------------------------------'
+		print 'Image Width: %d\nImage Height: %d\nBit Depth: %d\nChannel: %d'%(self.width,self.height,self.bits,self.channel)
+		print 'ColorType: %s'%(mode_dict[self.mode])
+		print 'Interlace: %s\nFilter method: %s\nCompression method: %s'%(interlace_dict[self.interlace],filter_dict[self.filter],compress_dict[self.compression])
+		print 'Content: '
+		for k in self.content:
+			if self.content[k]!=[]:
+				text_t='\n'.join(self.content[k]).split('\n')
+				text=''
+				import re
+				for t in text_t:
+					if re.match(r'^[ ]+$',t):
+						pass
+					else:
+						text+='\n'+t
+				print '%s: '%k,text
+		print '--------------------------------------------------------------------------------\n'
+
+	def ClearFilter(self,idat,width,height,channel,bits=8):
+		IDAT=''
+		if len(idat)==height*width*channel:
+			return idat
+		filter_unit=bits/8*channel
+		for i in range(0,len(idat),width*channel+1):
+			line_filter=str2num(idat[i])
+			idat_data=idat[i+1:i+width*channel+1]
+			if i>=1:
+				idat_data_u=tmp
+			else:
+				idat_data_u=[0]*width*channel
+
+			if line_filter not in [0,1,2,3,4]:
+				return -1
+
+			if line_filter==0: #None
+				tmp=list(idat_data)
+				IDAT+=''.join(tmp)
+
+			elif line_filter==1: #Sub
+				k=0
+				tmp=list(idat_data)
+				for j in range(filter_unit,len(idat_data)):
+					tmp[j] = chr((ord(idat_data[j]) + ord(tmp[k])) % 256)
+					k+=1
+				IDAT+=''.join(tmp)
+
+			elif line_filter==2: # Up
+				tmp=''
+				for j in range(len(idat_data)):
+					tmp += chr((ord(idat_data[j]) + ord(idat_data_u[j])) % 256)
+				IDAT+=tmp
+				tmp=list(tmp)
+
+			elif line_filter == 3: # Average
+				tmp=list(idat_data)
+				k=-filter_unit
+				for j in range(len(idat_data)):
+					if k<0:
+						a=0
+					else:
+						a=ord(tmp[k])
+					tmp[j] = chr((ord(idat_data[j]) + (a + ord(idat_data_u[j])) / 2) % 256)
+					k+=1
+				IDAT+=''.join(tmp)
+			
+			elif line_filter == 4: #Paeth
+				def predictor(a, b, c):
+					'''a = left, b = above, c = upper left'''
+					p = a + b -c
+					pa = abs(p - a)
+					pb = abs(p - b)
+					pc = abs(p - c)
+					if pa <= pb and pa <= pc:
+						return a
+					elif pb <= pc:
+						return b
+					else:
+						return c
+				k=-filter_unit
+				tmp=list(idat_data)
+				for j in range(len(idat_data)):
+					if k<0:
+						a=c=0
+					else:
+						a=ord(tmp[k])
+						c=ord(idat_data_u[k])
+					tmp[j] = chr((ord(idat_data[j]) + predictor(a, ord(idat_data_u[j]), c)) % 256 )
+					k+=1
+				IDAT+=''.join(tmp)
+		return IDAT
+		
+	def zlib_decrypt(self,data):
+		# Use in IDAT decompress
+		z_data=zlib.decompress(data)
+		return z_data
+
+	def LoadPNG(self):
 		data=ReadFile(self.in_file)
 		if data == -1:
 			return -1
 		status=self.CheckFormat(data)
 		if status == -1:
 			print Termcolor('Warning','The file may be not a PNG image.')
+			return -1
+		return data
+
+
+	def DecompressPNG(self,data,channel=3,bits=8,width=1,height=1):
+		# data: array[idat1,idat2,...]
+		from PIL import Image
+		IDAT_data=''
+		for idat in data:
+			IDAT_data+=idat
+		z_idat=self.zlib_decrypt(IDAT_data)
+		length=len(z_idat)
+
+		if width==0 and height == 0:
+			# bruteforce
+			import shutil
+			channel_dict={1:'L',3:'RGB',2:'LA',4:'RGBA'}
+			PATH='tmp/'
+			if os.path.isdir(PATH)==True:
+				shutil.rmtree(PATH)
+			os.mkdir(PATH)
+			for bits in [8,16]:
+				for channel in [4,3,1,2]:
+					size_list=[]
+					for i in range(1,length):
+						if length%i==0:
+							if (i-1)%(bits/8*channel)==0:
+								size_list.append((i-1)/(bits/8*channel))
+								size_list.append(length/i)
+							if (length/i-1)%(bits/8*channel)==0:
+								size_list.append((length/i-1)/(bits/8*channel))
+								size_list.append(i)
+					for i in range(0,len(size_list),2):
+						width=size_list[i]
+						height=size_list[i+1]
+						tmp=self.ClearFilter(z_idat,width,height,channel,bits)
+						if tmp!=-1:
+							img=Image.fromstring(channel_dict[channel],(width,height), tmp)
+							# img.show()
+							filename=PATH+'test(%dx%d)_%dbits_%dchannel.png'%(width,height,bits,channel)
+							img.save(filename)
+
+			# show all possible image
+			os.startfile(os.getcwd()+'/'+PATH)
+			# final size
+			size=raw_input('Input width, height, bits and channel(space to split):').split()
+			# remove temporary file
+			shutil.rmtree(PATH)
+
+			width=int(size[0])
+			height=int(size[1])
+			bits=int(size[2])
+			channel=int(size[3])
+			tmp=self.ClearFilter(z_idat,width,height,channel,bits)
+			if tmp==-1:
+				print 'Wrong'
+				return -1
+			img=Image.fromstring(channel_dict[channel],(width,height), tmp)
+			img.save('decompress.png')
+
+		else:
+			if width==1 and height == 1:
+				# load PNG config
+				status=self.GetPicInfo()
+				if status == -1:
+					return -1
+				width=self.width
+				height=self.height
+				channel=self.channel
+				bits=self.bits
+			else:
+				pass
+			z_idat=self.ClearFilter(z_idat,width,height,channel,bits)
+
+			mode_dict={0:'L',2:'RGB',3:'P',4:'LA',6:'RGBA'}
+			img=Image.fromstring(mode_dict[self.mode],(width,height), z_idat)
+			img.show()
+			img.save('zlib.png')
+
+		return 0
+
+
+	def FindAncillary(self,data):
+		ancillary=['cHRM','gAMA','sBIT','PLTE','bKGD','sTER','hIST','iCCP','pHYs','sPLT','sRGB','dSIG','eXIf','iTXt','tEXt','zTXt','tIME','tRNS','oFFs','sCAL','fRAc','gIFg','gIFt','gIFx']
+		attach_txt=['eXIf','iTXt','tEXt','zTXt']
+		content={}
+		for text in attach_txt:
+			pos=0
+			content[text]=[]
+			while pos!=-1:
+				pos=data.find(text,pos)
+				if pos!=-1:
+					length=str2num(data[pos-4:pos])
+					content[text].append(data[pos+4:pos+4+length])
+					pos+=1
+		return content
+
+
+	def CheckPNG(self):
+		data=self.LoadPNG()
+		if data==-1:
+			return -1
+
 		self.file=WriteFile(self.out_file)
 		res=self.CheckHeader(data)
 		if res == -1:
@@ -126,18 +425,22 @@ class PNG(object):
 		if res == -1:
 			print '[Finished] PNG check complete'
 			return -1
-		res=self.CheckIDAT(data)
+
+		res,idat=self.CheckIDAT(data)
 		if res == -1:
 			print '[Finished] PNG check complete'
 			return -1
 		self.CheckIEND(data)
 		print '[Finished] PNG check complete'
+
+		'''check complete'''
+
 		if self.choices != '':
 			choice=self.choices
 		else:
-			msg=Termcolor('Notice','Show the repaired image? (y or n) [default:y] ')
+			msg=Termcolor('Notice','Show the repaired image? (y or n) [default:n] ')
 			choice=raw_input(msg)
-		if choice == 'y' or choice == '':
+		if choice == 'y':
 			try:
 				from PIL import Image
 				self.file.close()
@@ -147,12 +450,6 @@ class PNG(object):
 				print Termcolor('Error',e)
 				print "Try 'pip install PIL' to use it"
 		return 0
-
-
-	def zlib_decrypt(self,data):
-		# Use in IDAT decompress
-		z_data=zlib.decompress(data)
-		return z_data
 
 	def Checkcrc(self,chunk_type,chunk_data, checksum):
 		# CRC-32 computed over the chunk type and chunk data, but not the length
@@ -191,23 +488,29 @@ class PNG(object):
 		self.file.write(Header)
 		return 0
 
-	def CheckIHDR(self,data):
-		# IHDR:length=13(4 bytes)+chunk_type='IHDR'(4 bytes)+chunk_ihdr(length bytes)+crc(4 bytes)
-		# chunk_ihdr=width(4 bytes)+height(4 bytes)+left(5 bytes)
+	def FindIHDR(self,data):
 		pos=data.find('IHDR')
 		if pos == -1:
-			print Termcolor('Detected','Lost IHDR chunk')
-			return -1
+			return -1,-1
 		idat_begin=data.find('IDAT')
 		if idat_begin != -1:
 			IHDR=data[pos-4:idat_begin-4]
 		else:
 			IHDR=data[pos-4:pos+21]
+		return pos,IHDR
+
+	def CheckIHDR(self,data):
+		# IHDR:length=13(4 bytes)+chunk_type='IHDR'(4 bytes)+chunk_ihdr(length bytes)+crc(4 bytes)
+		# chunk_ihdr=width(4 bytes)+height(4 bytes)+left(5 bytes)
+		pos,IHDR=self.FindIHDR(data)
+		if pos==-1:
+			print Termcolor('Detected','Lost IHDR chunk')
+			return -1
 		length=struct.unpack('!I',IHDR[:4])[0]
 		chunk_type=IHDR[4:8]
 		chunk_ihdr=IHDR[8:8+length]
-		width=struct.unpack('!I',chunk_ihdr[:4])[0]
-		height=struct.unpack('!I',chunk_ihdr[4:8])[0]
+
+		width,height=struct.unpack('!II',chunk_ihdr[:8])
 		crc=IHDR[8+length:12+length]
 		# check crc
 		calc_crc = self.Checkcrc(chunk_type,chunk_ihdr,crc)
@@ -240,6 +543,8 @@ class PNG(object):
 		self.file.write(IHDR)
 		print '[Finished] IHDR chunk check complete (offset: %s)'%(int2hex(pos-4))
 
+		# get image information
+		self.GetPicInfo(ihdr=chunk_ihdr)
 
 	def CheckIDAT(self,data):
 		# IDAT:length(4 bytes)+chunk_type='IDAT'(4 bytes)+chunk_data(length bytes)+crc(4 bytes)
@@ -247,8 +552,8 @@ class PNG(object):
 		idat_begin = data.find('49444154'.decode('hex'))-4
 		if idat_begin == -1:
 			print Termcolor('Detected','Lost all IDAT chunk!')
-			return -1
-		if self.mode == 0:
+			return -1,''
+		if self.i_mode == 0:
 			# fast: assume both chunk length are true
 			idat_size=struct.unpack('!I',data[idat_begin:idat_begin+4])[0]+12
 			for i in xrange(idat_begin,len(data)-12,idat_size): 
@@ -256,7 +561,7 @@ class PNG(object):
 			if i < len(data)-12:
 				# the last IDAT chunk
 				IDAT_table.append(data[i:-12])
-		elif self.mode == 1:
+		elif self.i_mode == 1:
 			# slow but safe
 			pos_IEND=data.find('IEND')
 			if pos_IEND != -1:
@@ -278,6 +583,7 @@ class PNG(object):
 				IDAT_table.append(data[pos1-4:pos2-4])
 
 		offset=idat_begin
+		IDAT_data_table=[]
 		for IDAT in IDAT_table:
 			length=struct.unpack('!I',IDAT[:4])[0]
 			chunk_type=IDAT[4:8]
@@ -322,9 +628,10 @@ class PNG(object):
 
 			# write into file
 			self.file.write(IDAT)
+			IDAT_data_table.append(chunk_data)
 			offset+=len(chunk_data)+12
 		print '[Finished] IDAT chunk check complete (offset: %s)'%(int2hex(idat_begin))
-		return 0
+		return 0,IDAT_data_table
 
 	def FixDos2Unix(self,chunk_type,chunk_data,crc,count):
 		pos=-1
@@ -401,13 +708,20 @@ if __name__=='__main__':
 
 Project address: https://github.com/sherlly/PCRT
 Author: sherlly
-Version: 1.0
-	"""
+Version: %s
+	"""%(__version__)
 
 	parser = argparse.ArgumentParser()
-	parser.add_argument('-q', action='store_true', help="don't show the banner infomation")
-	parser.add_argument('-y', help='auto choose yes',action='store_true')
+	parser.add_argument('-q', '--quiet',action='store_true', help="don't show the banner infomation")
+	parser.add_argument('-y', '--yes',help='auto choose yes',action='store_true')
 	parser.add_argument('-v', '--verbose', help='use the safe way to recover',action='store_true')
+	parser.add_argument('-m', '--message',help='show the image information',action='store_true')
+	parser.add_argument('-n', '--name', help="payload name [Default: random]")
+	parser.add_argument('-p', '--payload', help="payload to hide")
+	parser.add_argument('-w', '--way', type=int, default=1, help="payload chunk: [1]: ancillary [2]: critical [Default:1]")
+
+	parser.add_argument('-d', '--decompress', help="decompress zlib data file name")
+
 	parser.add_argument('-i', '--input', help='Input file name (*.png) [Select from terminal]')
 	parser.add_argument('-f', '--file', help='Input file name (*.png) [Select from window]',action='store_true')
 	parser.add_argument('-o', '--output', default='output.png', help='Output repaired file name [Default: output.png]')
@@ -415,35 +729,55 @@ Version: 1.0
 
 	in_file=args.input
 	out_file=args.output
-	
-	if args.q != True:
-		print msg
-	if args.verbose == True:
-		mode=1
-	else:
-		mode=0
+	payload=args.payload
+	payload_name=args.name
+	z_file=args.decompress
 
-	if args.file == True:
-		try:
-			import Tkinter
-			import tkFileDialog
-			root=Tkinter.Tk()
-			in_file=tkFileDialog.askopenfilename()
-			root.destroy()
-			if args.y == True:
+	if args.quiet != True:
+		print msg
+
+	if z_file!=None:
+		z_data=ReadFile(z_file)
+		my_png=PNG()
+		my_png.DecompressPNG(z_data,width=0,height=0)
+	else:
+		if args.verbose == True:
+			mode=1
+		else:
+			mode=0
+		if args.file == True:
+			try:
+				import Tkinter
+				import tkFileDialog
+				root=Tkinter.Tk()
+				in_file=tkFileDialog.askopenfilename()
+				root.destroy()
+				if args.yes == True:
+					my_png=PNG(in_file,out_file,choices='y',mode=mode)
+				else:
+					my_png=PNG(in_file,out_file,mode=mode)
+				if args.message == True:
+					my_png.PrintPicInfo()
+				elif payload != None:
+					way=args.way
+					my_png.AddPayload(payload_name,payload,way)
+				else:
+					my_png.CheckPNG()
+			except ImportError,e:
+				print Termcolor('Error',e)
+				print "Try 'pip install Tkinter' to use it"
+		elif in_file!=None:
+			if args.yes == True:
 				my_png=PNG(in_file,out_file,choices='y',mode=mode)
 			else:
 				my_png=PNG(in_file,out_file,mode=mode)
-			my_png.CheckPNG()
-		except ImportError,e:
-			print Termcolor('Error',e)
-			print "Try 'pip install Tkinter' to use it"
-	elif in_file!=None:
-		if args.y == True:
-			my_png=PNG(in_file,out_file,choices='y',mode=mode)
+			if args.message == True:
+				my_png.PrintPicInfo()
+			elif payload != None:
+				way=args.way
+				my_png.AddPayload(payload_name,payload,way)
+			else:
+				my_png.CheckPNG()
 		else:
-			my_png=PNG(in_file,out_file,mode=mode)
-		my_png.CheckPNG()
-	else:
-		parser.print_help()
+			parser.print_help()
 
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # PCRT (PNG Check & Repair Tool)
 [![Python 2.7](https://img.shields.io/badge/Python-2.7-blue.svg)](https://www.python.org/downloads/) 
-[![Version 1.0](https://img.shields.io/badge/Version-1.0-brightgreen.svg)]() 
+[![Version 1.1](https://img.shields.io/badge/Version-1.1-brightgreen.svg)]() 
 
 ## Description
 
@@ -8,6 +8,7 @@
 
 It can:
 
+	Show image information
 	Fix PNG header error
 	Fix wrong IHDR chunk crc due to error picture's width or height
 	Fix wrong IDAT chunk data length due to DOS->UNIX conversion
@@ -15,6 +16,8 @@ It can:
 	Fix lost IEND chunk
 	Extract data after IEND chunk (Malware programs like use this way to hide)
 	Show the repaired image
+	Inject payload into image
+	Decompress image data and show the raw image
 	...
 	Maybe more in the future :)  
 
@@ -39,23 +42,32 @@ It can:
 ## Usage
 
 	> python PCRT.py -h
-	usage: PCRT.py [-h] [-q] [-y] [-v] [-i INPUT] [-f] [-o OUTPUT]
+	usage: PCRT.py [-h] [-q] [-y] [-v] [-m] [-n NAME] [-p PAYLOAD] [-w WAY]
+                 [-d DECOMPRESS] [-i INPUT] [-f] [-o OUTPUT]
 
 	optional arguments:
-	  -h, --help            show this help message and exit
-	  -q                    don't show the banner infomation
-	  -y                    auto choose yes
-	  -v, --verbose         use the safe way to recover
-	  -i INPUT, --input INPUT
-	                        Input file name (*.png) [Select from terminal]
-	  -f, --file            Input file name (*.png) [Select from window]
-	  -o OUTPUT, --output OUTPUT
-	                        Output repaired file name [Default: output.png]
+    -h, --help            show this help message and exit
+    -q, --quiet           don't show the banner infomation
+    -y, --yes             auto choose yes
+    -v, --verbose         use the safe way to recover
+    -m, --message         show the image information
+    -n NAME, --name NAME  payload name [Default: random]
+    -p PAYLOAD, --payload PAYLOAD
+                          payload to hide
+    -w WAY, --way WAY     payload chunk: [1]: ancillary [2]: critical
+                          [Default:1]
+    -d DECOMPRESS, --decompress DECOMPRESS
+                          decompress zlib data file name
+    -i INPUT, --input INPUT
+                          Input file name (*.png) [Select from terminal]
+    -f, --file            Input file name (*.png) [Select from window]
+    -o OUTPUT, --output OUTPUT
+                          Output repaired file name [Default: output.png]
 
 **[Notice]** without `-v` option means that assume all IDAT chunk length are correct
 
 
-### Show
+## Show
 
 - Window:
 
@@ -69,7 +81,7 @@ It can:
 
 ![](http://i.imgur.com/re4gQux.png)
 
-### Some Problem:
+## Some Problem:
 
 - For Window:
 
@@ -80,6 +92,30 @@ It can:
 3. Add the new code: `return "start /wait %s && PING 127.0.0.1 -n 5 > NUL && del /f %s" % (file, file)` and save
 4. Restart the python IDE
 
+## Release Log
+
+### version 1.1:
+
+
+**Add：**
+
+- Show image information (`-m`)
+- Inject payload into image (`-p`)
+	- add into ancillary chunk (chunk name can be randomly generated or self-defined) (`-w 1`)
+	- add into critical chunk (only support IDAT chunk) (`-w 2`)
+- decompress image data and show the raw image (`-d`)
+
+### version 1.0：
+
+**Feature:**
+
+- Fix PNG header error
+- Fix wrong IHDR chunk crc due to error picture's width or height
+- Fix wrong IDAT chunk data length due to DOS->UNIX conversion
+- Fix wrong IDAT chunk crc due to its own error
+- Fix lost IEND chunk
+- Extract data after IEND chunk (Malware programs like use this way to hide)
+- Show the repaired image
 ---
 
 ## 项目描述
@@ -88,6 +124,7 @@ It can:
 
 它可以实现：
 
+	显示图片文件信息
 	修复PNG文件头错误
 	修复由于错误的图片长度或宽度导致的IHDR块crc校验出错
 	修复由于DOS->UNIX平台自动格式转换导致的部分IDAT块数据长度出错
@@ -95,6 +132,8 @@ It can:
 	修复丢失的IEND块
 	提取追加在IEND块后的数据（恶意程序常使用的传播方式）
 	自动显示修复后的图片
+	添加payload到图片文件
+	解析图片压缩数据并显示
 	...
 	更多功能有待开发：）
 	
@@ -120,23 +159,32 @@ It can:
 ## 使用方法
 
 	> python PCRT.py -h
-	usage: PCRT.py [-h] [-q] [-y] [-v] [-i INPUT] [-f] [-o OUTPUT]
+	usage: PCRT.py [-h] [-q] [-y] [-v] [-m] [-n NAME] [-p PAYLOAD] [-w WAY]
+                 [-d DECOMPRESS] [-i INPUT] [-f] [-o OUTPUT]
 
 	optional arguments:
-	  -h, --help            show this help message and exit
-	  -q                    don't show the banner infomation
-	  -y                    auto choose yes
-	  -v, --verbose         use the safe way to recover
-	  -i INPUT, --input INPUT
-	                        Input file name (*.png) [Select from terminal]
-	  -f, --file            Input file name (*.png) [Select from window]
-	  -o OUTPUT, --output OUTPUT
-	                        Output repaired file name [Default: output.png]
+    -h, --help            show this help message and exit
+    -q, --quiet           don't show the banner infomation
+    -y, --yes             auto choose yes
+    -v, --verbose         use the safe way to recover
+    -m, --message         show the image information
+    -n NAME, --name NAME  payload name [Default: random]
+    -p PAYLOAD, --payload PAYLOAD
+                          payload to hide
+    -w WAY, --way WAY     payload chunk: [1]: ancillary [2]: critical
+                          [Default:1]
+    -d DECOMPRESS, --decompress DECOMPRESS
+                          decompress zlib data file name
+    -i INPUT, --input INPUT
+                          Input file name (*.png) [Select from terminal]
+    -f, --file            Input file name (*.png) [Select from window]
+    -o OUTPUT, --output OUTPUT
+                          Output repaired file name [Default: output.png]
 
 **[注意]** 如果不添加 `-v` 选项则默认所有IDAT块长度均合法， 即不出现声明的IDAT数据块长度和实际数据长度不符合的情况。
 
 
-### 示例
+## 示例
 
 - Window:
 
@@ -150,7 +198,7 @@ It can:
 
 ![](http://i.imgur.com/re4gQux.png)
 
-### 可能出现的一些问题
+## 可能出现的一些问题
 
 - Window下：
 
@@ -160,3 +208,28 @@ It can:
 2. 将在100行左右的代码`return "start /wait %s && ping -n 2 127.0.0.1 >NUL && del /f %s" % (file, file)`注释掉
 3. 添加代码：`return "start /wait %s && PING 127.0.0.1 -n 5 > NUL && del /f %s" % (file, file)`并保存
 4. 重启运行python的IDE
+
+## 更新日志
+
+### version 1.1:
+
+
+**增加：**
+
+- 显示图片文件信息（`-m`）
+- 添加payload到图片文件（`-p`）
+	- 支持添加到辅助块（块名可随机生成或自定义）（`-w 1`）
+	- 支持添加到关键块（仅支持IDAT）(`-w 2`)
+- 解析图片压缩数据并显示（`-d`）
+
+### version 1.0：
+
+**特性：**
+
+- 实现修复PNG文件头错误
+- 实现修复由于错误的图片长度或宽度导致的IHDR块crc校验出错
+- 实现修复由于DOS->UNIX平台自动格式转换导致的部分IDAT块数据长度出错
+- 实现修复由于自身错误导致的IDAT块crc校验出错
+- 实现修复丢失的IEND块
+- 实现提取追加在IEND块后的数据（恶意程序常使用的传播方式）
+- 实现自动显示修复后的图片
